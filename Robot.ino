b590621b194262e2fbe7ad00737213f928e0542a
merlin <votatm@gmail.com>
2022-09-26 16:40:34 +0200

    fix offset

--- Robot.ino
+++ Robot.ino
@@ -4,15 +4,15 @@
 class AngleCoo
 {
   public:
-    int Gamma;
-    int Alpha;
-    int Beta;
+    float Gamma;
+    float Alpha;
+    float Beta;
     AngleCoo(){
       Gamma = 0;
       Alpha = 0;
       Beta = 0;
     }
-    AngleCoo(int gamma,int alpha,int beta){
+    AngleCoo(float gamma,float alpha,float beta){
       Gamma = gamma;
       Alpha = alpha;
       Beta = beta;
@@ -46,10 +46,10 @@ const int servo_pin[4][3] = {
 int offset[4][3];
 
 const int realPosition[4][3] = {
-  {96,52,43},
-  {93,63,30},
-  {111,64,61}, 
-  {94,65,65}
+  {102,53,53},
+  {92,67,42},
+  {107,80,88}, 
+  {101,58,60}
 };
 
 const int R2 = 0;
@@ -57,9 +57,9 @@ const int R1 = 1;
 const int L1 = 2;
 const int L2 = 3;
 
-const double LH = 30.5;
-const double LF = 53;
-const double LT = 79.5;
+const float LH = 30.5;
+const float LF = 53;
+const float LT = 79.5;
 
 void calculateOffset(){
   AngleCoo target = ConvertPointToAngle(100,70,42);
@@ -79,36 +79,36 @@ void calculateOffset(){
   Serial.println(realR2.Alpha);
   Serial.println(realR2.Beta);
 
-  offset[0][0] = realR2.Gamma < target.Gamma ? target.Gamma-realR2.Gamma : realR2.Gamma-target.Gamma;
-  offset[0][1] = realR2.Alpha > target.Alpha ? target.Alpha-realR2.Alpha : realR2.Alpha-target.Alpha;
-  offset[0][2] = realR2.Beta < target.Beta ? target.Beta-realR2.Beta : realR2.Beta-target.Beta;
+  offset[0][0] = target.Gamma-realR2.Gamma;
+  offset[0][1] = target.Alpha-realR2.Alpha;
+  offset[0][2] = target.Beta-realR2.Beta;
 
   Serial.println("R1");
   Serial.println(realR1.Gamma);
   Serial.println(realR1.Alpha);
   Serial.println(realR1.Beta);
 
-  offset[1][0] = realR1.Gamma < target.Gamma ? target.Gamma-realR1.Gamma : realR1.Gamma-target.Gamma;
-  offset[1][1] = realR1.Alpha > target.Alpha ? target.Alpha-realR1.Alpha : realR1.Alpha-target.Alpha;
-  offset[1][2] = realR1.Beta < target.Beta ? target.Beta-realR1.Beta : realR1.Beta-target.Beta;
+  offset[1][0] = target.Gamma-realR1.Gamma;
+  offset[1][1] = target.Alpha-realR1.Alpha;
+  offset[1][2] = target.Beta-realR1.Beta;
 
   Serial.println("L1");
   Serial.println(realL1.Gamma);
   Serial.println(realL1.Alpha);
   Serial.println(realL1.Beta);
     
-  offset[2][0] = realL1.Gamma < target.Gamma ? target.Gamma-realL1.Gamma : realL1.Gamma-target.Gamma;
-  offset[2][1] = realL1.Alpha < target.Alpha ? target.Alpha-realL1.Alpha : realL1.Alpha-target.Alpha;
-  offset[2][2] = realL1.Beta > target.Beta ? target.Beta-realL1.Beta : realL1.Beta-target.Beta;
+  offset[2][0] = target.Gamma-realL1.Gamma;
+  offset[2][1] = target.Alpha-realL1.Alpha;
+  offset[2][2] = target.Beta-realL1.Beta;
 
   Serial.println("L2");
   Serial.println(realL2.Gamma);
   Serial.println(realL2.Alpha);
   Serial.println(realL2.Beta);
     
-  offset[3][0] = realL2.Gamma < target.Gamma ? target.Gamma-realL2.Gamma : realL2.Gamma-target.Gamma;
-  offset[3][1] = realL2.Alpha > target.Alpha ? target.Alpha-realL2.Alpha : realL2.Alpha-target.Alpha;
-  offset[3][2] = realL2.Beta < target.Beta ? target.Beta-realL2.Beta : realL2.Beta-target.Beta;
+  offset[3][0] = target.Gamma-realL2.Gamma;
+  offset[3][1] = target.Alpha-realL2.Alpha;
+  offset[3][2] = target.Beta-realL2.Beta;
 
   for (int i = 0; i < 4; i++)
   {
@@ -139,19 +139,22 @@ void setPLS(){
 
 AngleCoo ConvertPointToAngle(float x, float y, float z){
   z -= 27;
-  int gamma = atan(y/x)*(180/M_PI);
-  int hypo = sqrt(pow(x,2)+pow(y,2));
-  int v = hypo - LH;
-  int d = sqrt(pow(v,2)+pow(z,2));
-  int alphaOne = atan(z / v) * (180/M_PI);
-  int alphaTwo = acos((pow(LF,2)+ pow(d,2) - pow(LT,2))/(2*LF*LT)) * (180/M_PI);
-  int alpha = alphaOne+alphaTwo;
-  int beta = acos((pow(LT,2)+pow(LF,2)-pow(d,2))/(2*LT*LF)) * (180/M_PI);
+  float gamma = atan(y/x)*(180/M_PI);
+  float hypo = sqrt(pow(x,2)+pow(y,2));
+  float v = hypo - LH;
+  float d = sqrt(pow(v,2)+pow(z,2));
+  float alphaOne = atan(z / v) * (180/M_PI);
+  float alphaTwo = acos((pow(d,2) + pow(LF,2) - pow(LT,2))/(2*LF*d)) * (180/M_PI);
+  float alpha = alphaOne+alphaTwo;
+  float beta = acos((pow(LF,2)+pow(LT,2)-pow(d,2))/(2*LT*LF)) * (180/M_PI);
   return AngleCoo(gamma, alpha, beta);
 }
 
 void move(int indexLeg, AngleCoo coo)
 {
+  coo.Gamma+offset[indexLeg][0];
+  coo.Alpha+offset[indexLeg][1];
+  coo.Beta+offset[indexLeg][2];
   switch (indexLeg)
   {
     case R2:
@@ -166,7 +169,7 @@ void move(int indexLeg, AngleCoo coo)
     case L1:
       coo.Gamma = 90 - coo.Gamma;
       coo.Alpha = 90 + coo.Alpha;
-      coo.Beta = 180 - coo.Beta;
+      coo.Beta = 205 - coo.Beta;
       break;
     case L2:
       coo.Gamma = 90 + coo.Gamma;
@@ -177,9 +180,9 @@ void move(int indexLeg, AngleCoo coo)
   }
 
   if(checkAngle(indexLeg, coo)){
-    servo[indexLeg][0].write(coo.Gamma+offset[indexLeg][0]);
-    servo[indexLeg][1].write(coo.Alpha+offset[indexLeg][1]);
-    servo[indexLeg][2].write(coo.Beta+offset[indexLeg][2]);
+    servo[indexLeg][0].write(coo.Gamma);
+    servo[indexLeg][1].write(coo.Alpha);
+    servo[indexLeg][2].write(coo.Beta);
   }else{
     setPLS();
   }
